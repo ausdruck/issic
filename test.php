72dd0ac9fe29ebcc7834b7b0c2272570434e5be9
seruice11002 <32900808+seruice11002@users.noreply.github.com>
2018-11-17 13:08:18 +0800

    不需要異動使用者

---
+++ test.php
@@ -0,0 +1,86 @@
+<?php
+$list=array("web3mac0001","web3mac0002","web3mac0003","weballway","weballway0001","webbcf0001","webbcf0002","webchener0002","webchener0003","webchener0005","webchener0006","webchener0007","webchener0008","webchener0009","webchener0010","webchener0011","webchener0012","webchener0013","webchener0014","webchener0015","webchungyi0001","webdigiori1056","webdigiori1057","webdqp0001","webdqp0002","webdqp0003","webdqp0004","webdqp0005","webendless0001","webendless0002","webendless0003","webendless0004","webezdo0001","webezdo0002","webezdo0003","webezdo0004","webeznlife0001","webhonesty0002","webhonesty0003","webjoy0001","webl0004","webl0006","webl0007","webl0008","webl0009","webm0007","webm0020","webm0022","webm0024","webm0025","webm0026","webm0027","webm0028","webm0030","webm0032","webm0033","webm0034","webm0035","webm0036","webm0037","webm0041","webm0042","webm0043","webniche0002","webniche0003","webplanimc0001","webplanimc0002","webpumacool0001","webpumacool0002","webpumacool0003","webpumacool0005","webpumacool0006","webseekfun0001","webseekfun0002","webseekfun0003","webseekfun0004","webseekfun0005","webseekfun0006","webshin0001","webshin0002","webshin0004","webshin0005","webshin0006","webshin0007","webshin0009","webshin0010","webshin0011","webshin0012","webshin0013","webshin0014","webshin0015","webshin0016","webshin0017","webshin0018","webshin0019","webshin0020","webshin0021","webshin0022","webshin0023","webshin0024","webshin0025","webswpak0001","webtehsing0001","webyes1040002","wolong0000","wolong0000eng","wolong0003","wolong0007","wolong0019","wolong0029","wolong0033","wolong0036","wolong0047","wolong0069","wolong0070","wolong0082","wolong0092","wolong0094","wolong0095","wolong0127","wolong0141","wolong0153","wolong0182","wolong0187","wolong0224","wolong0226","wolong0233","wolong0239","wolong0242","wolong0244","wolong0250","wolong0253","wolong0267","wolong0270","wolong0278","wolong0281","wolong0281_","wolong0289","wolong0303","wolong0327","wolong0334","wolong0339","wolong0353","wolong0356","wolong0357","wolong0367","wolong0368","wolong0369","wolong0379","wolong0394","wolong0395","wolong0396","wolong0408","wolong0409","wolong0411","wolong0427","wolong0461","wolong0467","wolong0472","wolong0473","wolong0475","wolong0478","wolong0484","wolong0489","wolong0494","wolong0501","wolong0502","wolong0507","wolong0509","wolong0511","wolong0512","wolong0519","wolong0521","wolong0529","wolong0530","wolong0534","wolong0537","wolong0539","wolong0540","wolong0546","wolong0557","wolong0564","wolong0568","wolong0570","wolong0574","wolong0580","wolong0581","wolong0588","wolong0592","wolong0597","wolong0600","wolong0607","wolong0623","wolong0633","wolong0635","wolong0636","wolong0650","wolong0653","wolong0663","wolong0673","wolong0677","wolong0711","wolong0716","wolong0728","wolong0735","wolong0735_eng","wolong0751","wolong0753","wolong0756","wolong0777","wolong0781","wolong0783","wolong0787","wolong0788","wolong0793","wolong0795","wolong0799","wolong0802","wolong0808","wolong0810","wolong0813","wolong0814","wolong0822","wolong0823","wolong0825","wolong0826","wolong0827","wolong0828","wolong0830","wolong0831","wolong0832","wolong0835","wolong0845","wolong0860","wolong0863","wolong0867","wolong0874","wolong0876","wolong0878","wolong0888","wolong0900","wolong0901","wolong0904","wolong0905","wolong0909","wolong0911","wolong0912","wolong0914","wolong0916","wolong0919","wolong0921","wolong0925","wolong0928","wolong0934","wolong0935","wolong0939","wolong0940","wolong0941","wolong0942","wolong0946","wolong0958","wolong0960","wolong0962","wolong0966","wolong0972","wolong0973","wolong0975","wolong0976","wolong0978","wolong0980","wolong0981","wolong0984","wolong0985","wolong0989","wolong0990","wolong0993","wolong0996","wolong0996_TEMP","wolong0999","wolong1005","wolong1008","wolong1009","wolong1011","wolong1012","wolong1014","wolong1016","wolong1024","wolong1031","wolong1032","wolong1035","wolong1039","wolong1043","wolong1046","wolong1048","wolong1052","wolong1055","wolong1064","wolong1066","wolong1067","wolong1069","wolong1070","wolong1071","wolong1073","wolong1078","wolong1079","wolong1081","wolong1084","wolong1085","wolong1086","wolong1089","wolong1091","wolong1093","wolong1096","wolong1097","wolong1098","wolong1100","wolong1103","wolong1106","wolong1107","wolong1108","wolong1113","wolong1115","wolong1118","wolong1119","wolong1120","wolong1121","wolong1122","wolong1127","wolong1129","wolong1131","wolong1132","wolong1133","wolong1134","wolong1135","wolong1136","wolong1137","wolong1139","wolong1140","wolong1142","wolong1143","wolong1144","wolong1145","wolong1146","wolong1147","wolong1148","wolong1149","wolong1151","wolong1154","wolong1155","wolong1156","wolong1157","wolong1158","wolong1160","wolong1161","wolong1162","wolong1163","wolong1164","wolong1165","wolong1166","wolong1167","wolong1168","wolong1169","wolong1170","wolong1171","wolong1173","wolong1174","wolong1175","wolong1176","wolong1177","wolong1178","wolong1180","wolong1181","wolong1182","wolong1183","wolong1184","wolong1185","wolong1186","wolong1187","wolong1189","wolong1190","wolong1192","wolong1195","wolong1196","wolong1197","wolong1198","wolong1202","wolong1203","wolong1205","wolong1206","wolong1207","wolong1208","wolong1209","wolong1210","wolong1211","wolong1212","wolong1213","wolong1214","wolong1216","wolong1217","wolong1220","wolong1221","wolong1222","wolong1223","wolong1224","wolong1225","wolong1226","wolong1227","wolong1229","wolong1230","wolong1231","wolong1232","wolong1233","wolong1235","wolong1241","wolong1242","wolong1243","wolong1245","wolong1247","wolong1249","wolong1252","wolong1253","wolong1256","wolong1257","wolong1259","wolong1261","wolong1262","wolong1272","wolong1274","wolong1275","wolong1276","wolong1278","wolong1279","wolong1280","wolong1281","wolong1282","wolong1284","wolong1285","wolong1286","wolong1287","wolong1288","wolong1290","wolong1291","wolong1292","wolong1294","wolong1295","wolong1297","wolong1298","wolong1300","wolong1307","wolong1308","wolong1309","wolong1312","wolong1313","wolong1314","wolong1316","wolong1317","wolong1318","wolong1319","wolong1322","wolong1323","wolong1324","wolong1325","wolong1327","wolong1328","wolong1329","wolong1331","wolong1332","wolong1333","wolong1334","wolong1335","wolong1336","wolong1338","wolong1339","wolong1340","wolong1341","wolong1342","wolong1344","wolong1345","wolong1346","wolong1347","wolong1350","wolong1351","wolong1353","wolong1355","wolong1356","wolong1357","wolong1359","wolong1360","wolong1363","wolong1364","wolong1365","wolong1366","wolong1367","wolong1368","wolong1369","wolong1370","wolong1371","wolong1372","wolong1373","wolong1374","wolong1375","wolong1376","wolong1377","wolong1378","wolong1379","wolong1382","wolong1383","wolong1384","wolong1385","wolong1386","wolong1387","wolong1391","wolong1394","wolong1395","wolong1397","wolong1398","wolong1399","wolong1400","wolong1401","wolong1402","wolong1403","wolong1404","wolong1405","wolong1406","wolong1407","wolong1408","wolong1409","wolong1410","wolong1411","wolong1412","wolong1413","wolong1414","wolong1415","wolong1416","wolong1417","wolong1418","wolong1419","wolong1420","wolong1421","wolong1422","wolong1424","wolong1425","wolong1426","wolong1427","wolong1428","wolong1429","wolong1430","wolong1431","wolong1432","wolong1433","wolong1442","wolong1443","wolong1444","wolong1445","wolong1446","wolong1447","wolong1447_img","wolong1448","wolong1449","wolong1450","wolong1451","wolong1452","wolong1453","wolong1454","wolong1455","wolong1457","wolong1458","wolong1459","wolong1460","wolong1461","wolong1463","wolong1465","wolong1466","wolong1467","wolong1468","wolong1469","wolong1470","wolong1471","wolong1474","wolong1475","wolong1476","wolong1477","wolong1478","wolong1479","wolong1480","wolong1481","wolong1482","wolong1483","wolong1484","wolong1485","wolong1486","wolong1487","wolong1488","wolong1490","wolong1492","wolong1493","wolong1495","wolong1497","wolong1498","wolong1499","wolong1500","wolong1501","wolong1502","wolong1503","wolong1504","wolong1505","wolong1506","wolong1507","wolong1509","wolong1510","wolong1511","wolong1512","wolong1513","wolong1515","wolong1516","wolong1519","wolong1520","wolong15201","wolong15202","wolong15203","wolong15204","wolong15205","wolong15206","wolong15207","wolong15208","wolong15209","wolong15210","wolong15211","wolong15212","wolong15213","wolong15214","wolong15215","wolong15217","wolong15218","wolong15219","wolong15220","wolong15221","wolong15222","wolong15223","wolong15224","wolong15225","wolong15226","wolong15227","wolong15229","wolong15230","wolong15231","wolong15232","wolong15233","wolong15234","wolong15235","wolong15236","wolong15237","wolong15238","wolong15239","wolong15240","wolong15241","wolong15242","wolong15243","wolong15244","wolong15246","wolong15247","wolong15248","wolong15249","wolong15252","wolong15253","wolong15254","wolong15255","wolong15256","wolong15257","wolong15258","wolong15259","wolong15260","wolong15261","wolong15262","wolong15264","wolong15265","wolong15267","wolong15269","wolong15270","wolong15271","wolong15272","wolong15273","wolong15274","wolong15275","wolong15276","wolong15277","wolong15278","wolong15279","wolong15280","wolong15281","wolong15282","wolong15283","wolong15285","wolong15286","wolong15287","wolong15289","wolong15290","wolong15291","wolong15292","wolong15293","wolong15294","wolong15295");
+$sfile=array("365.html","88bf.html","ca88.html","fun88.html","mocu.html","ms.html","pug.html","qg.html","sbf.html","w88.html","wed.html","wnsr.html","lbj.html");
+$i=0;
+foreach ($list as $value)
+{
+ $path="d:/ae1/".$value."/local/";
+ $src="d:/ae1/wolong15251/local/";
+ $getdatefile="d:/ae1/".$value."/about/";
+ if(fileatime($getdatefile))
+ {
+	 $new_date = fileatime($getdatefile);
+ }
+ else
+ {
+	 $new_date = strtotime("2015-07-02 01:09:34");
+ }
+ if($i==13)
+ {
+	 $i=0;
+ }
+  if(!is_readable($path))
+    {
+        if(is_file($path))
+        {
+		   echo $path." exist <br>";
+        }
+        else
+        {
+         copy_dir($src,$path,$new_date);
+        }
+		//copy index file
+		$path1="d:/ae1/".$value."/contact.php";
+		if(is_file($path1))
+		{
+			$path2="d:/ae1/".$value."/vote.php";	
+			if(is_file($path2))
+		    {
+				echo $path1." file exist!!";
+			}
+			else
+			{
+				copy($path . '/' . $sfile[$i],$path2);
+				touch("$path2",$new_date);
+				echo "$path2";
+			}
+		}
+		else
+		{
+			copy($path . '/' . $sfile[$i],$path1);
+			//$new_date = strtotime("2015-07-02 01:09:34");
+			touch("$path1",$new_date);
+			echo "$path1";
+		}
+    }
+  else
+    {
+        echo "path is here";
+    }
+	$i++;
+}
+function copy_dir($src,$dst,$new_date) {
+	
+    //$new_date = strtotime("2015-07-02 01:09:34");
+    $dir = opendir($src);
+    @mkdir($dst,0755);
+    touch($dst,$new_date);
+    while(false !== ( $file = readdir($dir)) ) {
+        if (( $file != '.' ) && ( $file != '..' )) {
+            if ( is_dir($src . '/' . $file) ) {
+                copy_dir($src . '/' . $file,$dst . '/' . $file);
+                touch("$dst/$file",$new_date);
+                continue;
+            }
+            else {
+                copy($src . '/' . $file,$dst . '/' . $file);
+                touch("$dst/$file",$new_date);
+            }
+        }
+    }
+    touch($dst,$new_date);
+    closedir($dir);
+}
+
+?>
+
