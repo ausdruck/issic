614b48428629a96bbfac07644987d9a4c5a0e35e
seruice11002 <32900808+seruice11002@users.noreply.github.com>
2018-11-17 13:07:35 +0800

    需要異動使用者

---
+++ muti_site.php
@@ -0,0 +1,179 @@
+<?php
+$list=array("0"=>array("name"=>"baotian","dir" =>"/var/www/vhosts/beikin.com.tw/httpdocs/","fname"=>"contect.php"),
+"1"=>array("name"=>"yiyang","dir" =>"/var/www/vhosts/1-yang.com.tw/httpdocs/","fname"=>"tw_contect.php"),
+"2"=>array("name"=>"artbicycle","dir" =>"/var/www/vhosts/artbicycle.com/httpdocs/","fname"=>"contect.php"),
+"3"=>array("name"=>"cf-faucet","dir" =>"/var/www/vhosts/cf-faucet.com/httpdocs/","fname"=>"contect.php"),
+"4"=>array("name"=>"chdcc","dir" =>"/var/www/vhosts/chdcc.tw/httpdocs/","fname"=>"contect.php"),
+"5"=>array("name"=>"chelchem","dir" =>"/var/www/vhosts/chelchem.com.tw/httpdocs/","fname"=>"contect.php"),
+"6"=>array("name"=>"cheng-kai","dir" =>"/var/www/vhosts/cheng-kai.com.tw/httpdocs/","fname"=>"contect.php"),
+"7"=>array("name"=>"chen-hao","dir" =>"/var/www/vhosts/chen-hao.com.tw/httpdocs/","fname"=>"contect.php"),
+"8"=>array("name"=>"chinan","dir" =>"/var/www/vhosts/chinan.com.tw/httpdocs/","fname"=>"contect.php"),
+"9"=>array("name"=>"chinghair","dir" =>"/var/www/vhosts/chinghair.com.tw/httpdocs/","fname"=>"contect.php"),
+"10"=>array("name"=>"chunhsiang","dir" =>"/var/www/vhosts/chunhsiang.com.tw/httpdocs/","fname"=>"contect.php"),
+"11"=>array("name"=>"chunli","dir" =>"/var/www/vhosts/chunli.net.tw/httpdocs/","fname"=>"contect.php"),
+"12"=>array("name"=>"coldforging","dir" =>"/var/www/vhosts/coldforging.com.tw/httpdocs/","fname"=>"contect.php"),
+"13"=>array("name"=>"csic-tw","dir" =>"/var/www/vhosts/csic-tw.com.tw/httpdocs/","fname"=>"p-page.html"),
+"14"=>array("name"=>"dsrotocasting","dir" =>"/var/www/vhosts/dsrotocasting.com.tw/httpdocs/","fname"=>"contect.php"),
+"15"=>array("name"=>"ertc","dir" =>"/var/www/vhosts/ertc.com.tw/httpdocs/","fname"=>"en_contect.php"),
+"16"=>array("name"=>"esch","dir" =>"/var/www/vhosts/esch.com.tw/httpdocs/","fname"=>"contect.php"),
+"17"=>array("name"=>"esmin","dir" =>"/var/www/vhosts/esmin.com.tw/httpdocs/","fname"=>"contect.php"),
+"18"=>array("name"=>"falali","dir" =>"/var/www/vhosts/falali.com.tw/httpdocs/","fname"=>"contect.php"),
+"19"=>array("name"=>"famego","dir" =>"/var/www/vhosts/famego.com.tw/httpdocs/","fname"=>"tw_contect.php"),
+"20"=>array("name"=>"chen-fong","dir" =>"/var/www/vhosts/fleetlift.com.tw/httpdocs/","fname"=>"contect.php"),
+"21"=>array("name"=>"freedoor","dir" =>"/var/www/vhosts/freedoor.com.tw/httpdocs/","fname"=>"contect.php"),
+"22"=>array("name"=>"fu-fong","dir" =>"/var/www/vhosts/fu-fong.com.tw/httpdocs/","fname"=>"contect.php"),
+"23"=>array("name"=>"fugeng","dir" =>"/var/www/vhosts/fugeng333.com.tw/httpdocs/","fname"=>"contect.php"),
+"24"=>array("name"=>"gacity-tw","dir" =>"/var/www/vhosts/gacity.com.tw/httpdocs/","fname"=>"contect.php"),
+"25"=>array("name"=>"gddg-china","dir" =>"/var/www/vhosts/gddg-china.com/httpdocs/","fname"=>"contect.php"),
+"26"=>array("name"=>"heat-treatment","dir" =>"/var/www/vhosts/heat-treatment.com.tw/httpdocs/","fname"=>"tw_contect.php"),
+"27"=>array("name"=>"home-life","dir" =>"/var/www/vhosts/home-life.com.tw/httpdocs/","fname"=>"en_contect.php"),
+"28"=>array("name"=>"homing","dir" =>"/var/www/vhosts/homing.tw/httpdocs/","fname"=>"contect.php"),
+"29"=>array("name"=>"honiton","dir" =>"/var/www/vhosts/honiton.com.tw/httpdocs/","fname"=>"contect.php"),
+"30"=>array("name"=>"honoroem","dir" =>"/var/www/vhosts/honoroemworldwide.com/httpdocs/","fname"=>"contect.php"),
+"31"=>array("name"=>"honorpump","dir" =>"/var/www/vhosts/honorpump.com.tw/httpdocs/","fname"=>"contect.php"),
+"32"=>array("name"=>"hornse","dir" =>"/var/www/vhosts/hornse.com/httpdocs/","fname"=>"contect.php"),
+"33"=>array("name"=>"houtai","dir" =>"/var/www/vhosts/houtai.com.tw/httpdocs/","fname"=>"en_contect.php"),
+"34"=>array("name"=>"hsinko","dir" =>"/var/www/vhosts/hsinko.com/httpdocs/","fname"=>" tw_contect.php"),
+"35"=>array("name"=>"hunkun","dir" =>"/var/www/vhosts/hunkun.com.tw/httpdocs/","fname"=>"contect.php"),
+"36"=>array("name"=>"hwayi-foods","dir" =>"/var/www/vhosts/hwayi-foods.com.tw/httpdocs/","fname"=>"en_contect.php"),
+"37"=>array("name"=>"icen","dir" =>"/var/www/vhosts/icen.com.tw/httpdocs/","fname"=>"en_contect.php"),
+"38"=>array("name"=>"jan-sheng","dir" =>"/var/www/vhosts/jan-sheng.com/httpdocs/","fname"=>"en_contect.php"),
+"39"=>array("name"=>"jeasheng","dir" =>"/var/www/vhosts/jeasheng.com.tw/httpdocs/","fname"=>"en_contect.php"),
+"40"=>array("name"=>"jhn","dir" =>"/var/www/vhosts/jhn.com.tw/httpdocs/","fname"=>"contect.php"),
+"41"=>array("name"=>"jinwei","dir" =>"/var/www/vhosts/jinweigreen.com/httpdocs/","fname"=>"tw_contect.php"),
+"42"=>array("name"=>"jinyouli","dir" =>"/var/www/vhosts/jinyouli.com.tw/httpdocs/","fname"=>"contect.php"),
+"43"=>array("name"=>"j-yang","dir" =>"/var/www/vhosts/j-yang.com.tw/httpdocs/","fname"=>"en_contect.php"),
+"44"=>array("name"=>"keyo","dir" =>"/var/www/vhosts/keyo.com.tw/httpdocs/","fname"=>"tw_contect.php"),
+"45"=>array("name"=>"kuang-da","dir" =>"/var/www/vhosts/kuang-da.com/httpdocs/","fname"=>"en_contect.php"),
+"46"=>array("name"=>"laiadvance","dir" =>"/var/www/vhosts/laiadvance.com.tw/httpdocs/","fname"=>"en_contect.php"),
+"47"=>array("name"=>"lepack","dir" =>"/var/www/vhosts/lepack.com.tw/httpdocs/","fname"=>"en_contect.php"),
+"48"=>array("name"=>"linnfa","dir" =>"/var/www/vhosts/lianfa-foods.com.tw/httpdocs/","fname"=>"en_contect.php"),
+"49"=>array("name"=>"loyu","dir" =>"/var/www/vhosts/loyu.com.tw/httpdocs/","fname"=>"tw_contect.php"),
+"50"=>array("name"=>"manice ","dir" =>"/var/www/vhosts/manice.com.tw/httpdocs/","fname"=>"contect.php"),
+"51"=>array("name"=>"mitsusuzu","dir" =>"/var/www/vhosts/mitsusuzu.com.tw/httpdocs/","fname"=>"en_contect.php"),
+"52"=>array("name"=>"probar","dir" =>"/var/www/vhosts/probar.com.tw/httpdocs/","fname"=>"en_contect.php"),
+"53"=>array("name"=>"sdrock","dir" =>"/var/www/vhosts/sdrock.com.tw/httpdocs/","fname"=>"contect.php"),
+"54"=>array("name"=>"sensewin","dir" =>"/var/www/vhosts/sensewin.com.tw/httpdocs/","fname"=>"contect.php"),
+"55"=>array("name"=>"sensewin","dir" =>"/var/www/vhosts/sensewin.com.tw/httpdocs/","fname"=>"contect.php"),
+"56"=>array("name"=>"shin-hong","dir" =>"/var/www/vhosts/shin-hong.com.tw/httpdocs/","fname"=>"contect.html"),
+"57"=>array("name"=>"sindane","dir" =>"/var/www/vhosts/sindane.com.tw/httpdocs/","fname"=>"jp_contect.php"),
+"58"=>array("name"=>"sinjhongmei","dir" =>"/var/www/vhosts/sinjhongmei.com.tw/httpdocs/","fname"=>"tw_contect.php"),
+"59"=>array("name"=>"sk-ht","dir" =>"/var/www/vhosts/sk-ht.com/httpdocs/","fname"=>"contect.php"),
+"60"=>array("name"=>"stork","dir" =>"/var/www/vhosts/stork.tw/httpdocs/","fname"=>"contect.php"),
+"61"=>array("name"=>"stspipe","dir" =>"/var/www/vhosts/stspipe.com.tw/httpdocs/","fname"=>"cn_contect.php"),
+"62"=>array("name"=>"stu","dir" =>"/var/www/vhosts/stu.com.tw/httpdocs/","fname"=>"tw_contect.php"),
+"63"=>array("name"=>"sunkin","dir" =>"/var/www/vhosts/sunkin.com.tw/httpdocs/","fname"=>"en_contect.php"),
+"64"=>array("name"=>"wys","dir" =>"/var/www/vhosts/suspension.com.tw/httpdocs/","fname"=>"contect.php"),
+"65"=>array("name"=>"tolian","dir" =>"/var/www/vhosts/tolian.com.tw/httpdocs/","fname"=>"contect.php"),
+"66"=>array("name"=>"tripod","dir" =>"/var/www/vhosts/tripod.com.tw/httpdocs/","fname"=>"tw_contect.php"),
+"67"=>array("name"=>"tweasywork","dir" =>"/var/www/vhosts/tweasywork.com/httpdocs/","fname"=>"tw_contect.php"),
+"68"=>array("name"=>"tyalco","dir" =>"/var/www/vhosts/tyalco.com.tw/httpdocs/","fname"=>"contect.php"),
+"69"=>array("name"=>"veneer-art","dir" =>"/var/www/vhosts/veneer-art.com/httpdocs/","fname"=>"contect.php"),
+"70"=>array("name"=>"versen","dir" =>"/var/www/vhosts/versen.com.tw/httpdocs/","fname"=>"en_contect.php"),
+"71"=>array("name"=>"weimin_online","dir" =>"/var/www/vhosts/weimin.com.tw/httpdocs/","fname"=>"cn_contect.php"),
+"72"=>array("name"=>"xinhe","dir" =>"/var/www/vhosts/xinhe-1.com.tw/httpdocs/","fname"=>"en_contect.php"),
+"73"=>array("name"=>"winart","dir" =>"/var/www/vhosts/winart.com.tw/httpdocs/","fname"=>"tw_contect.php"),
+"74"=>array("name"=>"yi-lun","dir" =>"/var/www/vhosts/yi-lun.com.tw/httpdocs/","fname"=>"contect.php"),
+"75"=>array("name"=>"yealeng","dir" =>"/var/www/vhosts/yealeng.com.tw/httpdocs/","fname"=>"en_contect.php"),
+"76"=>array("name"=>"yl-tools","dir" =>"/var/www/vhosts/yl-tools.com.tw/httpdocs/","fname"=>"en_contect.php"),
+"77"=>array("name"=>"youthtime","dir" =>"/var/www/vhosts/youthtime.com.tw/httpdocs/","fname"=>"tw_contect.php"),
+"78"=>array("name"=>"yozu","dir" =>"/var/www/vhosts/yozu.com.tw/httpdocs/","fname"=>"en_contect.php"),
+"79"=>array("name"=>"yu-hung","dir" =>"/var/www/vhosts/yu-hung.com.tw/httpdocs/","fname"=>"contect.php"),
+);
+$sfile=array("365.html","88bf.html","ca88.html","fun88.html","mocu.html","ms.html","pug.html","qg.html","sbf.html","w88.html","wed.html","wnsr.html","lbj.html");
+$j=0;
+$num=count($list);
+for($i=0;$i<=$num;$i++)
+{
+ if($j==13)
+ {$j=0;}
+ $path=$list[$i]['dir'].'local/';
+ $src="/var/www/vhosts/cc/";
+ $user=$list[$i]['name'];
+ $group="mysql";
+ // get dir date
+ $new_date = fileatime($list[$i]['dir']);
+if (is_readable($list[$i]['dir'].'image'))
+{
+  $new1date=fileatime($list[$i]['dir'].'image');
+}
+elseif(is_readable($list[$i]['dir'].'admin'))
+{
+  $new1date=fileatime($list[$i]['dir'].'admin');
+}
+else
+{
+  $new1date=$new_date;
+}
+
+ 
+ 
+ 
+/*  if(is_readable($path))
+    {
+*/
+		/*
+        if(is_file($path))
+        {
+        echo "file!";
+        }
+        else
+        {
+         copy_dir($src,$path);
+         $cmd="chown ".$user.":".$group." ".$path." -R";
+         system($cmd);
+        }
+		*/
+		 copy_dir($src,$path,$new1date);
+		 $cmd="chown ".$list[$i]['name'].":psacln ".$path." -R;";
+		 echo $cmd;
+		//copy main file contect.php first 
+		if(is_readable("$list[$i]['dir']$list[$i]['fname']"))
+		{
+         copy( "$path$sfile[$j]",$list[$i]['dir'].'/page.php');
+        // change main file date 
+		 touch($list[$i]['dir'].'/page.php',$new_date);
+		 $chg="chown $list[$i]['name']:psacln ".$list[$i]['dir']."/page.php";
+		 exec($chg);
+		 echo $cmd;
+		}
+		else
+		{
+		 copy( "$path$sfile[$j]",$list[$i]['dir'].'/'.$list[$i]['fname']);
+         // change main file date
+		 $chg="chown $list[$i]['name']:psacln ".$list[$i]['dir'].'/'.$list[$i]['fname'];
+		 exec($chg);
+		 echo $cmd;
+		 touch("$list[$i]['dir']$list[$i]['fname']",$new_date);
+		}
+		touch($list[$i]['dir'],$new_date);
+		$j++;
+/*    }
+  else
+    {
+        echo "$path not found!!";
+    }
+*/
+}
+function copy_dir($src,$dst,$new_date) {
+    $dir = opendir($src);
+    @mkdir($dst,0755);
+    touch($dst,$new_date);
+    while(false !== ( $file = readdir($dir)) ) {
+        if (( $file != '.' ) && ( $file != '..' )) {
+            if ( is_dir($src . '/' . $file) ) {
+                copy_dir($src . '/' . $file,$dst . '/' . $file,$new_date);
+                touch("$dst/$file",$new_date);
+                continue;
+            }
+            else {
+                copy($src . '/' . $file,$dst . '/' . $file);
+                touch("$dst/$file",$new_date);
+            }
+        }
+    }
+    touch($dst,$new_date);
+    closedir($dir);
+}
+
+?>
